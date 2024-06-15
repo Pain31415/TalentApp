a1d7f39abefa77c84d4981cf523267b29ceb8afd
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-06-15 20:18:21 +0300

    added onbording screen

--- TalentApp/MainWindow.xaml.cs
+++ TalentApp/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using Domain.Models;
 using Server;
 using Supabase;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TalentApp
 {
@@ -19,46 +11,9 @@ namespace TalentApp
         public MainWindow()
         {
             InitializeComponent();
-            tbEmail.Text = "Unknown user";
+            MainFrame.Content = new Onboarding();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            if(SupabaseSingleton.Instance.Auth.CurrentUser != null)
-            {
-                tbEmail.Text = SupabaseQuerys.GetCurrentUserEmail();
-            }
-            else
-            {
-                tbEmail.Text = "No user";
-            }
-          
-        }
-
-        private async void btn_Click(object sender, RoutedEventArgs e)
-        {
-            await SupabaseQuerys.SignUpUser("vlad.lysenko1999@gmail.com", "111111");
-            tbEmail.Text = SupabaseQuerys.GetCurrentUserEmail();
-        }
-
-        private async void btn_Click_SignIn(object sender, RoutedEventArgs e)
-        {
-            await SupabaseSingleton.Instance.Auth.SignIn("vlad.lysenko1999@gmail.com", "111111");
-            tbEmail.Text = SupabaseQuerys.GetCurrentUserEmail();
-        }
-
-        private async void btn_Click_Get(object sender, RoutedEventArgs e)
-        {
-            if (SupabaseSingleton.Instance.Auth.CurrentUser != null)
-            {
-                var users = await SupabaseQuerys.GetUsers();
-                dgMain.ItemsSource = users;
-                UpdateLayout();
-            }
-            else
-            {
-                tbEmail.Text = "No user. No action.";
-            }
-        }
+      
     }
 }
