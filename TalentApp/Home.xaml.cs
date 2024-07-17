909c5c765a511b9afe63977cc04e420087e99901
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-07-17 17:48:58 +0300

    New window Home

---
+++ TalentApp/Home.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace TalentApp
+{
+    public partial class Home : Window
+    {
+        private bool isSidebarExpanded = false;
+
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleSidebarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isSidebarExpanded)
+            {
+                CollapseSidebar();
+            }
+            else
+            {
+                ExpandSidebar();
+            }
+
+            isSidebarExpanded = !isSidebarExpanded;
+        }
+
+        private void ExpandSidebar()
+        {
+            Storyboard expandStoryboard = (Storyboard)FindResource("ExpandSidebar");
+            expandStoryboard.Begin();
+        }
+
+        private void CollapseSidebar()
+        {
+            Storyboard collapseStoryboard = (Storyboard)FindResource("CollapseSidebar");
+            collapseStoryboard.Begin();
+        }
+    }
+}
--- TalentApp/Window13.xaml.cs
+++ TalentApp/Window13.xaml.cs
@@ -16,6 +16,15 @@ namespace TalentApp
             await Task.Delay(5000); // 5 seconds delay
             LoadingPanel.Visibility = Visibility.Collapsed;
             SuccessPanel.Visibility = Visibility.Visible;
+
+            await Task.Delay(2000); // 2 seconds delay after showing success
+            NavigateToHome();
+        }
+
+        private void NavigateToHome()
+        {
+            var parentWindow = Window.GetWindow(this);
+            parentWindow.Content = new Home();
         }
     }
 }
