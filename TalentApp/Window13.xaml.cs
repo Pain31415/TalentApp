bf931b90f1c5944dd82efffe3bf8cd8c24cb19fe
Влад Потук <vladpotuk01@gmail.com>
2024-07-17 18:20:28 +0300

    гзвфеу

--- TalentApp/Window13.xaml.cs
+++ TalentApp/Window13.xaml.cs
@@ -13,18 +13,19 @@ namespace TalentApp
 
         private async void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            await Task.Delay(5000); // 5 seconds delay
+            await Task.Delay(5000); 
             LoadingPanel.Visibility = Visibility.Collapsed;
             SuccessPanel.Visibility = Visibility.Visible;
 
-            await Task.Delay(2000); // 2 seconds delay after showing success
+            await Task.Delay(2000); 
             NavigateToHome();
         }
 
         private void NavigateToHome()
         {
             var parentWindow = Window.GetWindow(this);
-            parentWindow.Content = new Home();
+            Home homeWindow = new Home();
+            parentWindow.Content = homeWindow.Content;
         }
     }
 }
