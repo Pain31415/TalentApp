06845f2fe5508209662344eeebfaa58809f08098
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-08-27 18:18:06 +0300

    up

---
+++ TalentApp/Domain/UseCase.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using Server;
+
+namespace TalentApp.Domain
+{
+    public class UseCase
+    {
+        public static async Task<List<User>> GetUsers()
+        {
+            return await SupabaseQuerys.GetUsers();
+        }
+    }
+}
--- TalentApp/Home.xaml.cs
+++ TalentApp/Home.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System.Windows;
+﻿using Domain.Models;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
+using TalentApp.Domain;
 
 namespace TalentApp
-{ 
+{
     public partial class Home : Window
     {
         private bool isSidebarExpanded = false;
@@ -11,6 +13,7 @@ namespace TalentApp
         public Home()
         {
             InitializeComponent();
+            
         }
 
         private void ToggleSidebarButton_Click(object sender, RoutedEventArgs e)
@@ -37,6 +40,108 @@ namespace TalentApp
         {
             Storyboard collapseStoryboard = (Storyboard)FindResource("CollapseSidebar");
             collapseStoryboard.Begin();
+        }
+
+        private void NavigationButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            string tag = button.Tag.ToString();
+
+            UserControl contentControl = null;
+            Window window = null;
+
+            switch (tag)
+            {
+                case "Home":
+                    contentControl = new HomeControl();
+                    break;
+                case "Documents":
+                    contentControl = new DocumentsControl();
+                    break;
+                case "Mail":
+                    contentControl = new MailControl();
+                    break;
+                case "User":
+                    contentControl = new UserControlControl();
+                    break;
+                case "HR":
+                    contentControl = new HRControl();
+                    break;
+                case "Settings":
+                    window = new SettingsWindow();
+                    break;
+                default:
+                    return;
+            }
+
+            if (window != null)
+            {
+                window.Show();
+                this.Close();
+            }
+            else
+            {
+                MainContent.Content = contentControl;
+            }
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AppDataGrid.ItemsSource = await UseCase.GetUsers();
+        }
+    }
+
+    // Зразкові класи для заміни відсутніх типів UserControl
+    public class HomeControl : UserControl
+    {
+      
+        public HomeControl()
+        {
+            DataGrid dataGrid = new DataGrid();
+            dataGrid.ItemsSource = UseCase.GetUsers().Result;
+            Content =dataGrid;
+        }
+    }
+
+    public class DocumentsControl : UserControl
+    {
+        public DocumentsControl()
+        {
+            Content = new TextBlock { Text = "Documents Content", Foreground = System.Windows.Media.Brushes.White };
+        }
+    }
+
+    public class MailControl : UserControl
+    {
+        public MailControl()
+        {
+            Content = new TextBlock { Text = "Mail Content", Foreground = System.Windows.Media.Brushes.White };
+        }
+    }
+
+    public class UserControlControl : UserControl
+    {
+        public UserControlControl()
+        {
+            Content = new TextBlock { Text = "User Content", Foreground = System.Windows.Media.Brushes.White };
+        }
+    }
+
+    public class HRControl : UserControl
+    {
+        public HRControl()
+        {
+            Content = new TextBlock { Text = "HR Content", Foreground = System.Windows.Media.Brushes.White };
+        }
+    }
+
+    public class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            Title = "Settings Window";
+            Width = 800;
+            Height = 600;
         }
     }
 }
